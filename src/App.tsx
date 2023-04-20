935a417927111a1d9d27b97a1b1ac626977c9118
anais <anaisboninfr@gmail.com>
2023-04-21 00:34:15 +0200

    create basic routes and Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
