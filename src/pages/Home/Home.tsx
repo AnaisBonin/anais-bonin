844996e52d7b1a99be6ccd85326d71e51e4b02d0
anais <anaisboninfr@gmail.com>
2023-06-10 01:46:44 +0200

    display HomeStars if title isClicked

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
+import HomeStars from '../../components/HomeStars/HomeStars';
+
 import { HeartIcon } from '../../assets/icons';
 
 import './Home.css';
 
 const Home = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => setIsClicked(!isClicked);
+
     return (
         <div className="home">
-            <h1 className="title">
+            <h1 className="title" onClick={handleClick}>
                 Bienvenue, Hello
             </h1>
             <HeartIcon color={"#70243B"} margin={"10px 20px"} />
+            {isClicked && <HomeStars />}
         </div>
+
     );
 };
 
