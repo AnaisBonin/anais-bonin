5db8c89aba966dc91c5d541a7c763ab41911b712
anais <anaisboninfr@gmail.com>
2023-06-20 16:26:03 +0200

    Integrate homeStar into Home for simplicity

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
-import HomeStars from '../../components/HomeStars/HomeStars';
-
+import Star from '../../components/Star/Star';
 import { HeartIcon } from '../../assets/icons';
 
 import './Home.css';
@@ -11,13 +10,24 @@ const Home = () => {
 
     const handleClick = () => setIsClicked(!isClicked);
 
+    const visibility = isClicked ? "visible" : "hidden";
+
+    const starsQuantity = [1];
+    for (let i = 1; i < 10; i++) {
+        starsQuantity.push(i + 1);
+    }
+
     return (
         <div className="home">
             <h1 className="title" onClick={handleClick}>
                 Bienvenue, Hello
             </h1>
             <HeartIcon color={"#70243B"} margin={"10px 20px"} />
-            <HomeStars visibility={isClicked ? "visible" : "hidden"} />
+            <div className={`home-stars ${visibility}`}>
+                {starsQuantity.map(star => (
+                    <Star number={star} />
+                ))}
+            </div>
         </div>
 
     );
