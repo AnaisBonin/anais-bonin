acec9af433ee3e5a4bb43a3322f5cfb4bd7f9d2b
anais <anaisboninfr@gmail.com>
2023-06-20 19:54:33 +0200

    EDIT Home and HomeTitle - move down changeTitle and resetTitle

--- src/components/HomeTitle/HomeTitle.tsx
+++ src/components/HomeTitle/HomeTitle.tsx
@@ -2,11 +2,34 @@ import './HomeTitle.css';
 
 interface HomeTitleProps {
     title?: string,
-    handleClick: () => void
+    handleClick: () => void,
+    setTitle: (newTitle?: string) => void
 }
 
-const HomeTitle = ({ title = 'Bienvenue, Hello', handleClick }: HomeTitleProps) => {
+const HomeTitle = ({ title = 'Bienvenue, Hello', handleClick, setTitle }: HomeTitleProps) => {
     const onClick = () => handleClick();
+
+    const resetTitle = () => setTitle(undefined);
+
+    const changeTitle = (star?: number) => {
+        if (star) {
+            switch (star) {
+                case 1:
+                    setTitle('Star 1')
+                    break;
+                case 3:
+                    setTitle('Star 3')
+                    break;
+                case 6:
+                    setTitle('Star 6')
+                    break;
+                default:
+                    resetTitle();
+            }
+        } else {
+            resetTitle();
+        }
+    }
 
     return (
         <h1 className="title" onClick={onClick}>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,34 +12,13 @@ const Home = () => {
     const [titleClicked, setTitleClicked] = useState(false);
     const [starSelected, setStarSelected] = useState<number>();
 
-    const resetTitle = () => setTitle(undefined);
     const handleTitleClick = () => setTitleClicked(!titleClicked);
-
-    const changeTitle = (star?: number) => {
-        if (star) {
-            switch (star) {
-                case 1:
-                    setTitle('Star 1')
-                    break;
-                case 3:
-                    setTitle('Star 3')
-                    break;
-                case 6:
-                    setTitle('Star 6')
-                    break;
-                default:
-                    resetTitle();
-            }
-        } else {
-            resetTitle();
-        }
-    }
 
     return (
         <div className="home">
-            <HomeTitle title={title} handleClick={handleTitleClick} />
+            <HomeTitle title={title} handleClick={handleTitleClick} setTitle={setTitle} />
             <HeartIcon color={"#70243B"} margin={"10px 20px"} />
-            <HomeStars setStarSelected={setStarSelected} display={titleClicked} />
+            <HomeStars display={titleClicked} setStarSelected={setStarSelected} />
         </div>
 
     );
