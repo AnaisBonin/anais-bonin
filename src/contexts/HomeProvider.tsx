2a4930dcb64c2ecab7649a26975b42bf58543ee5
anais <anaisboninfr@gmail.com>
2023-06-25 23:10:28 +0200

    EDIT HomeProvider: add title state, improve homeContext, export useHome

--- src/contexts/HomeProvider.tsx
+++ src/contexts/HomeProvider.tsx
@@ -1,34 +1,35 @@
 import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
 
 interface HomeStatesProps {
-    starSelected: string,
-    handleStarSelection: (name: string) => void,
+	starSelected: string;
+	handleStarSelection: (name: string) => void;
 }
 
-interface HomeProviderProps {
-    children: ReactNode,
+export interface HomeProviderProps {
+	children: ReactNode;
 }
 
 const HomeContext = createContext<HomeStatesProps>({
-    starSelected: '',
-    handleStarSelection (name) {},
+	starSelected: '',
+	handleStarSelection(name) {},
 });
 
 const HomeProvider = ({ children }: HomeProviderProps) => {
-    const [starSelected, setStarSelected] = useState('');
+	const [starSelected, setStarSelected] = useState('');
 
-    const handleStarSelection = (name: string) => setStarSelected(name)
+	const handleStarSelection = (name: string) => setStarSelected(name);
 
-    const HomeStates: HomeStatesProps = useMemo(() => ({
-        starSelected,
-        handleStarSelection,
-    }), [starSelected]);
+	const HomeStates: HomeStatesProps = useMemo(
+		() => ({
+			starSelected,
+			handleStarSelection,
+		}),
+		[starSelected]
+	);
 
-    return (
-        <HomeContext.Provider value={HomeStates}>
-            {children}
-        </HomeContext.Provider>
-    );
+	return (
+		<HomeContext.Provider value={HomeStates}>{children}</HomeContext.Provider>
+	);
 };
 
 export const useHome = () => useContext(HomeContext);
