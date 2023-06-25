3b3c9dd3f1ed0eb693b827acfba8833b8a453079
anais <anaisboninfr@gmail.com>
2023-06-26 00:09:48 +0200

    EDIT Home and Home Title: clean component, useProvider

--- src/components/HomeTitle/HomeTitle.tsx
+++ src/components/HomeTitle/HomeTitle.tsx
@@ -1,41 +1,16 @@
+import { useHome } from '../../contexts/HomeProvider';
+
 import './HomeTitle.css';
 
-interface HomeTitleProps {
-    title?: string,
-    handleClick: () => void,
-    setTitle: (newTitle?: string) => void
-}
 
-const HomeTitle = ({ title = 'Bienvenue, Hello', handleClick, setTitle }: HomeTitleProps) => {
-    const onClick = () => handleClick();
-
-    const resetTitle = () => setTitle(undefined);
-
-    const changeTitle = (star?: number) => {
-        if (star) {
-            switch (star) {
-                case 1:
-                    setTitle('Star 1')
-                    break;
-                case 3:
-                    setTitle('Star 3')
-                    break;
-                case 6:
-                    setTitle('Star 6')
-                    break;
-                default:
-                    resetTitle();
-            }
-        } else {
-            resetTitle();
-        }
-    }
+const HomeTitle = () => {
+    const { title, handleTitleClicked } = useHome();
 
     return (
-        <h1 className="title" onClick={onClick}>
+        <h1 className="title" onClick={handleTitleClicked}>
             {title}
         </h1>
     )
-}
+};
 
 export default HomeTitle
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import HomeProvider from '../../contexts/HomeProvider';
 import HomeStars from '../../components/HomeStars/HomeStars';
 import HomeTitle from '../../components/HomeTitle/HomeTitle';
 
@@ -8,18 +9,16 @@ import { HeartIcon } from '../../assets/icons';
 import './Home.css';
 
 const Home = () => {
-    const [title, setTitle] = useState<string>();
-    const [titleClicked, setTitleClicked] = useState(false);
     const [starSelected, setStarSelected] = useState<number>();
 
-    const handleTitleClick = () => setTitleClicked(!titleClicked);
-
     return (
-        <div className="home">
-            <HomeTitle title={title} handleClick={handleTitleClick} setTitle={setTitle} />
-            <HeartIcon color={"#70243B"} margin={"10px 20px"} />
-            <HomeStars display={titleClicked} setStarSelected={setStarSelected} />
-        </div>
+        <HomeProvider>
+            <div className="home">
+                <HomeTitle />
+                <HeartIcon color={"#70243B"} margin={"10px 20px"} />
+                <HomeStars setStarSelected={setStarSelected} />
+            </div>
+        </HomeProvider>
 
     );
 };
