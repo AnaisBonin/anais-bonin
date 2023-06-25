e81921c96494c8a832e9eab30f4882776e457cb1
anais <anaisboninfr@gmail.com>
2023-06-26 00:48:46 +0200

    EDIT HomeProvider: add starSelected state

--- src/contexts/HomeProvider.tsx
+++ src/contexts/HomeProvider.tsx
@@ -3,9 +3,11 @@ import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
 export interface HomeValuesProps {
     defaultTitle: string,
     isTitleClicked: boolean,
+    starSelected?: number,
     title: string,
-    handleTitleClicked: () => void,
     changeTitle: (value: string) => void,
+    handleStarSelected: (star?: number) => void,
+    handleTitleClicked: () => void,
 }
 
 export interface HomeProviderProps {
@@ -15,27 +17,33 @@ export interface HomeProviderProps {
 const HomeContext = createContext<HomeValuesProps>({
     defaultTitle: '',
     isTitleClicked: false,
+    starSelected: undefined,
     title: '',
-    handleTitleClicked: () => { },
     changeTitle: (title: string) => title,
+    handleStarSelected: (star?: number) => star,
+    handleTitleClicked: () => { },
 });
 
 const HomeProvider = ({ children }: HomeProviderProps) => {
     const defaultTitle = 'Hi there!';
 
-    const [title, setTitle] = useState(defaultTitle);
     const [isTitleClicked, setIsTitleClicked] = useState(false);
+    const [starSelected, setStarSelected] = useState<number>();
+    const [title, setTitle] = useState(defaultTitle);
 
     const changeTitle = (value: string) => setTitle(value);
     const handleTitleClicked = () => setIsTitleClicked(!isTitleClicked);
+    const handleStarSelected = (star?: number) => setStarSelected(star)
 
     const HomeValues: HomeValuesProps = useMemo(() => ({
         defaultTitle,
         isTitleClicked,
+        starSelected,
         title,
         changeTitle,
+        handleStarSelected,
         handleTitleClicked,
-    }), [isTitleClicked, title]);
+    }), [isTitleClicked, starSelected, title]);
 
     return (
         <HomeContext.Provider value={HomeValues}>
