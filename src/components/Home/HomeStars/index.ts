24cf845776b42550a877ad89e4bed7007c265264
anais <anaisboninfr@gmail.com>
2023-06-26 11:54:04 +0200

    ADD index.ts files in components, contexts and pages

---
+++ src/components/Home/HomeStars/index.ts
@@ -0,0 +1,1 @@
+export { default as HomeStars } from './HomeStars' 
--- src/components/Home/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as HomeStars } from './HomeStars/HomeStars';
-export { default as HomeTitle } from './HomeTitle/HomeTitle';
