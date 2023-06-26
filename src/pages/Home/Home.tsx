b321a94943e7c0ab845d6b881fd7f7cc93eb826c
anais <anaisboninfr@gmail.com>
2023-06-26 14:43:17 +0200

    FORMAT - Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,19 +5,14 @@ import { HeartIcon } from '../../assets/icons';
 
 import './Home.css';
 
-const Home = () => {
-
-
-    return (
-        <HomeProvider>
-            <div className="home">
-                <HomeTitle />
-                <HeartIcon color={"#70243B"} margin={"10px 20px"} />
-                <HomeStars />
-            </div>
-        </HomeProvider>
-
-    );
-};
+const Home = () => (
+	<HomeProvider>
+		<div className="home">
+			<HomeTitle />
+			<HeartIcon color={'#70243B'} margin={'10px 20px'} />
+			<HomeStars />
+		</div>
+	</HomeProvider>
+);
 
 export default Home;
