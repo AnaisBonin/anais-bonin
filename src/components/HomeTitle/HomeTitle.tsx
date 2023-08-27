28cb8e7cac999ea0229fa6ca05e1beaca6f98451
AnaisBonin <anaisboninfr@gmail.com>
2023-08-27 21:07:18 +0200

    Prettier fix on Hometitle and Home

--- src/components/HomeTitle/HomeTitle.tsx
+++ src/components/HomeTitle/HomeTitle.tsx
@@ -5,28 +5,30 @@ import { useHome } from '../../contexts/HomeProvider';
 import './HomeTitle.css';
 
 interface HomeTitleProps {
-    handleClick: () => void,
+	handleClick: () => void;
 }
 
 const HomeTitle = ({ handleClick }: HomeTitleProps) => {
-const { starSelected } = useHome();
+	const { starSelected } = useHome();
 
-const defaultTitle = 'Hello';
-const [title, setTitle] = useState<string>(defaultTitle);
+	const defaultTitle = 'Hello';
+	const [title, setTitle] = useState<string>(defaultTitle);
 
-    useEffect(() => {
-        if (starSelected === '') {
-            setTitle(defaultTitle);
-        } else {
-            setTitle(starSelected);
-        }
-    }, [starSelected, setTitle]);
+	useEffect(() => {
+		if (starSelected === '') {
+			setTitle(defaultTitle);
+		} else {
+			setTitle(starSelected);
+		}
+	}, [starSelected, setTitle]);
 
-    return (
-        <h1 className="title" onClick={handleClick}>
-            {title}
-        </h1>
-    )
-}
+	return (
+		<h1
+			className="title"
+			onClick={handleClick}>
+			{title}
+		</h1>
+	);
+};
 
-export default HomeTitle
+export default HomeTitle;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,18 +8,20 @@ import { HeartIcon } from '../../assets/icons';
 import './Home.css';
 
 const Home = () => {
-    const [titleClicked, setTitleClicked] = useState(false);
+	const [titleClicked, setTitleClicked] = useState(false);
 
-    const handleTitleClick = () => setTitleClicked(!titleClicked);
+	const handleTitleClick = () => setTitleClicked(!titleClicked);
 
-    return (
-        <div className="home">
-            <HomeTitle handleClick={handleTitleClick} />
-            <HeartIcon color={"#70243B"} margin={"10px 20px"} />
-            <HomeStars display={titleClicked} />
-        </div>
-
-    );
+	return (
+		<div className="home">
+			<HomeTitle handleClick={handleTitleClick} />
+			<HeartIcon
+				color={'#70243B'}
+				margin={'10px 20px'}
+			/>
+			<HomeStars display={titleClicked} />
+		</div>
+	);
 };
 
 export default Home;
