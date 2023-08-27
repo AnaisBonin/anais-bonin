5531753114af530bd0d3c62d60e1a4e296d5b53f
AnaisBonin <anaisboninfr@gmail.com>
2023-08-27 21:07:59 +0200

    EDIT portfolioSections file with section name and position

--- src/utils/portfolioSections.ts
+++ src/utils/portfolioSections.ts
@@ -1,1 +1,54 @@
-export const portfolioSections = ['Who am I', 'Experiences', 'Tech Skills', 'Soft Skills', 'Professional goals', 'Contact'];
+export interface IStarPosition {
+	top: string;
+	left: string;
+}
+
+export interface IPortfolioSections {
+	title: string;
+	position: IStarPosition;
+}
+
+export const portfolioSections: IPortfolioSections[] = [
+	{
+		title: 'Who am I',
+		position: {
+			top: '4vh',
+			left: '-45vw',
+		},
+	},
+	{
+		title: 'Experiences',
+		position: {
+			top: '15vh',
+			left: '-22vw',
+		},
+	},
+	{
+		title: 'Tech Skills',
+		position: {
+			top: '25vh',
+			left: '3vw',
+		},
+	},
+	{
+		title: 'Soft Skills',
+		position: {
+			top: '5vh',
+			left: '25vw',
+		},
+	},
+	{
+		title: 'Professional goals',
+		position: {
+			top: '33vh',
+			left: '40vw',
+		},
+	},
+	{
+		title: 'Contact',
+		position: {
+			top: '46vh',
+			left: '-43vw',
+		},
+	},
+];
