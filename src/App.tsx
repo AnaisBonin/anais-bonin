9d02552e7826ad721f16b382eea2ecd401d2d28e
AnaisBonin <anaisboninfr@gmail.com>
2023-08-27 18:21:44 +0200

    BUG FIX: add HomeProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import HomeProvider from './contexts/HomeProvider';
 import Home from './pages/Home/Home';
 
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <HomeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </HomeProvider>
   );
 }
 
