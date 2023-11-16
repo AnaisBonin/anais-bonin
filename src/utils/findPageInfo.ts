7e65263d28b94353bea0b70cd164d58e3f4ed7b5
anais <anaisboninfr@gmail.com>
2023-11-16 18:05:41 +0100

    EDIT util findPageInfo: return pageInfo or homePageInfo

--- src/utils/findPageInfo.ts
+++ src/utils/findPageInfo.ts
@@ -1,6 +1,6 @@
-import { IPortfolioPage, portfolioPages } from './portfolioPages'
+import { IHomePage, IPortfolioPage, homePage, portfolioPages } from './portfolioPages'
 
-export const findPageInfo = (title: string): IPortfolioPage | {} => {
+export const findPageInfo = (title: string): IHomePage | IPortfolioPage => {
 const pageInfo = portfolioPages.find((page) => page.title === title);
-return pageInfo ?? {};
+return pageInfo ?? homePage;
 }
