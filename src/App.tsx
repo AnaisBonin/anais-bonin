b983a3bbc9641450ce6332f34bc48c6bae76c79e
anais <anaisboninfr@gmail.com>
2023-11-16 18:39:00 +0100

    CREATE Index for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomeProvider from './contexts/HomeProvider';
-import Contact from './pages/Contact/Contact';
-import { Home } from './pages';
+import { Contact, Home } from './pages';
 
 import './App.css';
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,7 @@
-export * from './Home'
+export { default as About} from './About'
+export { default as Contact} from './Contact'
+export { default as Experiences} from './Experiences'
+export { default as Goals} from './Goals'
+export { default as Home} from './Home'
+export { default as SoftSkills} from './SoftSkills'
+export { default as TechSkills} from './TechSkills'
