42f84da2e94bb4ad9e2e0cfbeaa7c44aff03c7c8
anais <anaisboninfr@gmail.com>
2023-11-17 15:29:47 +0100

    EDIT Error css

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,13 +1,14 @@
 import { ReturnHome } from '../../components';
 
+import './Error.css';
+
 const Error = () => {
 	return (
 		<main>
-			<h1>Error</h1>
-			<p>
-				Lost in space? We get it, stars are pretty sick. Though if you want to
-				get back to us, return to our spaceship
-			</p>
+			<h1 className="page-title">OoOops</h1>
+			<p>Lost in the middle of 404 stars? </p>
+			<p>We get it, adventuring through space is pretty sick.</p>
+			<p>Though if you want to get back to us, get back to our spaceship</p>
 			<ReturnHome />
 		</main>
 	);
