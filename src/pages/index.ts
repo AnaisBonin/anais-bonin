a615871c1af8b27d04c90f29ccba021e7aa34012
anais <anaisboninfr@gmail.com>
2023-11-17 15:09:13 +0100

    CREATE Error page and index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as About} from './About'
 export { default as Contact} from './Contact'
+export { default as Error} from './Error'
 export { default as Experiences} from './Experiences'
 export { default as Goals} from './Goals'
 export { default as Home} from './Home'
