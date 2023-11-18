b3ff10f7023b713d39ad2ed482a19e3ca23f6533
anais <anaisboninfr@gmail.com>
2023-11-18 11:02:01 +0100

    ADD PageMenu to layout

--- src/layouts/EnterUniverse/EnterUniverse.tsx
+++ src/layouts/EnterUniverse/EnterUniverse.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
 import './EnterUniverse.css';
-import { ReturnHome } from '../../components';
+import { PageMenu, ReturnHome } from '../../components';
 
 const EnterUniverse = () => {
 	return (
-		<main className="main-universe">
-			<Outlet />
-			<ReturnHome />
-		</main>
+		<div className="main-universe">
+			<main>
+				<Outlet />
+			</main>
+			<footer className="align-h">
+				<ReturnHome />
+				<PageMenu />
+			</footer>
+		</div>
 	);
 };
 
