02690c5de6bcd32f6c1271dfaa5bc0f6e36261eb
anais <anaisboninfr@gmail.com>
2023-11-18 12:50:10 +0100

    ADD returnHome interaction on hover: change icon, set state

--- src/components/ReturnHome/ReturnHome.tsx
+++ src/components/ReturnHome/ReturnHome.tsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useHome } from '../../contexts/HomeProvider';
 
-import { RocketIcon } from '../../assets';
+import { RocketFiredIcon, RocketIcon } from '../../assets';
 
 import './ReturnHome.css';
 
-const ReturnHome = () => {
+interface IReturnHome {
+	setRocketState?: (value: boolean) => void;
+}
+
+const ReturnHome = ({ setRocketState }: IReturnHome) => {
 	const { handleStarSelection } = useHome();
+
+	const [isRocketOn, setIsRocketOn] = useState(false);
+
+	const fireRocket = () => setIsRocketOn(true);
+	const stopRocket = () => setIsRocketOn(false);
 
 	const resetStarSelection = () => handleStarSelection('');
 
+	useEffect(() => {
+		if (setRocketState) {
+			setRocketState(isRocketOn);
+		}
+	}, [isRocketOn]);
+
 	return (
-		<Link to="/" className="return-home-button" onClick={resetStarSelection}>
-			<RocketIcon color={'#DDCDD3'} margin={'0 5px'} />
+		<Link
+			to="/"
+			className="return-home-button"
+			onClick={resetStarSelection}
+			onMouseEnter={fireRocket}
+			onMouseLeave={stopRocket}
+		>
+			{isRocketOn ? (
+				<RocketFiredIcon margin={'0 5px'} />
+			) : (
+				<RocketIcon margin={'0 5px'} />
+			)}
 		</Link>
 	);
 };
