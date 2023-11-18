6e13db9c08d6a86eec764883e97725f1df26440e
anais <anaisboninfr@gmail.com>
2023-11-18 12:51:02 +0100

    EDIT enterUniverse: listen to rocketFired, if rocket on: display menu

--- src/layouts/EnterUniverse/EnterUniverse.tsx
+++ src/layouts/EnterUniverse/EnterUniverse.tsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import './EnterUniverse.css';
 import { PageMenu, ReturnHome } from '../../components';
 
 const EnterUniverse = () => {
+	const [isRocketOn, setIsRocketOn] = useState(false);
+
+	const setRocketState = (value: boolean) => setIsRocketOn(value);
+
 	return (
 		<div className="main-universe lato">
 			<main>
 				<Outlet />
 			</main>
 			<footer className="align-h">
-				<ReturnHome />
-				<PageMenu />
+				<ReturnHome setRocketState={setRocketState} />
+				{isRocketOn && <PageMenu />}
 			</footer>
 		</div>
 	);
