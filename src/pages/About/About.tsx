61d03a29fe5ab2a57e7841a3444157102229a8f0
anais <anaisboninfr@gmail.com>
2023-11-18 16:05:35 +0100

    EDIT About page: add content and text

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,7 +3,68 @@ import './About.css';
 const About = () => {
 	return (
 		<div>
-			<h1>About</h1>
+			<h1>About me</h1>
+			<h2>Hi!</h2>
+			<p>
+				I am a French woman, in my early thirties, loving the outdoor which we
+				can fully enjoy here in the Alps but also in so many places in France
+				and in Europe.
+			</p>
+			<p>
+				I am based near Grenoble, a beautiful city surrounded by Alpine
+				mountains. Grenoble has some of the best engineers and incredible
+				research institutes that are reknowned internationally (check our&nbsp;
+				<a href="https://www.esrf.fr/ " className="word-emph" target="_blank">
+					Synchrotron
+				</a>
+				, it is quite amazing).
+			</p>
+			<p>
+				It is also very well connected ( Direct lines to Geneva or Lyon
+				airports, 3h direct train to Paris, 1h20 to Lyon, 3h drive to
+				Marseille... ), and only 30 min to go skiing :).
+			</p>
+			<h2> Professional life</h2>
+			<p>
+				I did many things in my twenties. Moved to Sweden and Canada, studied,
+				worked, travelled...
+			</p>
+			<p>
+				As a kid and a teen, I was much more of a scientist. My focus was on
+				science, maths, physics, biology. But then I got curious about the world
+				we live in and studied more of social, psychological and managerial
+				sciences. I graduated with a{' '}
+				<strong>Master's degree in Business Administration</strong> (MBA)
+				specialized in <strong>Management</strong> (both team and project) and{' '}
+				<strong>Communications.</strong>
+			</p>
+			<p>
+				And obviously, as someone who loves puzzle and technologies, I became
+				very into creating digital tools.
+			</p>
+			<p>
+				After some years, I missed the technical parts of science, working with
+				abstract concepts, algorithms, solving problems and intricate puzzles...
+			</p>
+			<p>
+				That's why I decided to dedicate my whole time and the rest of my
+				carrier to create, develop, code web applications. Went back to
+				studying, started with <strong>Javascript React</strong>,{' '}
+				<strong>Node.js</strong>, <strong>Typescript</strong>,{' '}
+				<strong>SQL</strong>... (I already knew HTML and CSS from my studies and
+				learning them on my freetime). And then I expended my knowledge with{' '}
+				<strong>Java</strong> and so on.
+			</p>
+			<p>
+				I will answer know a question everyone keep asking: "are you more into
+				frontend or backend?" Honestly, I know you hate that but... both! I
+				really like working with React and having fun with CSS. But, building
+				servers and working with databases is also amazing. It is good to be
+				able to switch sometime from one to the other :)
+			</p>
+			<p>
+				Oh one last thing. <span className="word-emph">Welcome!</span> :)
+			</p>
 		</div>
 	);
 };
