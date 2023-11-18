2883365dfec79da65ab1245b555bc647650233ab
anais <anaisboninfr@gmail.com>
2023-11-18 13:44:28 +0100

    EDIT CSS footer layout: show/hide menu on rocket/menu hover

--- src/components/PageMenu/PageMenu.tsx
+++ src/components/PageMenu/PageMenu.tsx
@@ -4,9 +4,15 @@ import { portfolioPages } from '../../utils';
 
 import './PageMenu.css';
 
-const PageMenu = () => {
+interface IPageMenu {
+	display: boolean;
+}
+
+const PageMenu = ({ display }: IPageMenu) => {
+	const visibility = display ? 'visible' : 'hidden';
+
 	return (
-		<menu className="page-menu align-h cursor-flower">
+		<menu className={'page-menu align-h cursor-flower ' + visibility}>
 			{portfolioPages.map((page) => (
 				<li className="menu-item">
 					<Link to={page.path}>{page.title}</Link>
--- src/layouts/EnterUniverse/EnterUniverse.tsx
+++ src/layouts/EnterUniverse/EnterUniverse.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import './EnterUniverse.css';
 import { PageMenu, ReturnHome } from '../../components';
+
+import './EnterUniverse.css';
 
 const EnterUniverse = () => {
 	const [isRocketOn, setIsRocketOn] = useState(false);
+	const [displayMenu, setDisplayMenu] = useState(false);
+
+	const showMenu = () => setDisplayMenu(true);
+	const hideMenu = () => setDisplayMenu(false);
 
 	const setRocketState = (value: boolean) => setIsRocketOn(value);
+
+	useEffect(() => {
+		if (isRocketOn) {
+			showMenu();
+		}
+	}, [isRocketOn]);
 
 	return (
 		<div className="main-universe lato">
 			<main>
 				<Outlet />
 			</main>
-			<footer className="align-h">
+			<footer className="footer-universe align-h">
 				<ReturnHome setRocketState={setRocketState} />
-				{isRocketOn && <PageMenu />}
+				<div onMouseEnter={showMenu} onMouseLeave={hideMenu}>
+					<PageMenu display={displayMenu} />
+				</div>
 			</footer>
 		</div>
 	);
