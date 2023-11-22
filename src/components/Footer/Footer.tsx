317889f2359105f14272adb0941555ef45dc0adb
anais <anaisboninfr@gmail.com>
2023-11-22 11:52:17 +0100

    EDIT layout: move footer to its own component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import { PageMenu } from '../PageMenu';
+import { ReturnHome } from '../ReturnHome';
+
 import './Footer.css';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	const [isRocketOn, setIsRocketOn] = useState(false);
+	const [displayMenu, setDisplayMenu] = useState(false);
+
+	const showMenu = () => setDisplayMenu(true);
+	const hideMenu = () => setDisplayMenu(false);
+
+	const setRocketState = (value: boolean) => setIsRocketOn(value);
+
+	useEffect(() => {
+		if (isRocketOn) {
+			showMenu();
+		}
+	}, [isRocketOn]);
+
+	return (
+		<footer className="footer-universe align-h">
+			<ReturnHome setRocketState={setRocketState} />
+			<div onMouseEnter={showMenu} onMouseLeave={hideMenu}>
+				<PageMenu display={displayMenu} />
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/layouts/EnterUniverse/EnterUniverse.tsx
+++ src/layouts/EnterUniverse/EnterUniverse.tsx
@@ -1,36 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { PageMenu, ReturnHome } from '../../components';
+import { Footer } from '../../components';
 
 import './EnterUniverse.css';
 
 const EnterUniverse = () => {
-	const [isRocketOn, setIsRocketOn] = useState(false);
-	const [displayMenu, setDisplayMenu] = useState(false);
-
-	const showMenu = () => setDisplayMenu(true);
-	const hideMenu = () => setDisplayMenu(false);
-
-	const setRocketState = (value: boolean) => setIsRocketOn(value);
-
-	useEffect(() => {
-		if (isRocketOn) {
-			showMenu();
-		}
-	}, [isRocketOn]);
-
 	return (
 		<div className="main-universe lato">
 			<main>
 				<Outlet />
 			</main>
-			<footer className="footer-universe align-h">
-				<ReturnHome setRocketState={setRocketState} />
-				<div onMouseEnter={showMenu} onMouseLeave={hideMenu}>
-					<PageMenu display={displayMenu} />
-				</div>
-			</footer>
+			<Footer />
 		</div>
 	);
 };
